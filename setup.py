7817b26a5fa9491204355dccec0fcc00e39cb063
Andrean Franc <andrean.franc@gmail.com>
2016-03-07 11:45:41 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+import greentasks as pkg
+
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+VERSION = pkg.__version__
+
+setup(
+    name='greentasks',
+    version=VERSION,
+    license='BSD',
+    packages=[pkg.__name__],
+    include_package_data=True,
+    long_description=read('README.rst'),
+    install_requires=[
+        'gevent',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Applicaton',
+        'Framework :: Bottle',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
